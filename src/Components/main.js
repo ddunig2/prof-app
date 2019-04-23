b3a4e7ba3715260aee6e9103c622b868d46da7dc
Lugduni Desrosiers <36016544+ddunig2@users.noreply.github.com>
2019-04-23 12:19:04 -0400

    Update main.js

--- src/Components/main.js
+++ src/Components/main.js
@@ -3,22 +3,24 @@ import React from "react";
 import Home from "../pages/home";
 import LoanCalculator from "../pages/loan_calculator";
 import My_cars from "../pages/my_cars";
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import projects from "../pages/projects";
 import Experience from "../pages/experience";
 import Contact_me from "../pages/contact_me";
 import Full_resume from "../pages/resume";
 
 const Main = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/my_cars" component={My_cars} />
-    <Route path="/loanCalculator" component={LoanCalculator} />
-    <Route path="/projects" component={projects} />
-    <Route path="/experience" component={Experience} />
-    <Route path="/contact_me" component={Contact_me} />
-    <Route path="/full_resume" component={Full_resume} />
-  </Switch>
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/my_cars" component={My_cars} />
+      <Route exact path="/loanCalculator" component={LoanCalculator} />
+      <Route exact path="/projects" component={projects} />
+      <Route exact path="/experience" component={Experience} />
+      <Route exact path="/contact_me" component={Contact_me} />
+      <Route exact path="/full_resume" component={Full_resume} />
+    </Switch>
+  </HashRouter>
 );
 
 export default Main;
