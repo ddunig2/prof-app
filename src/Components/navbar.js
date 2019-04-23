db9d144bbea49c4ba8ba45e7fd4d14b10f7eb81a
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-04-23 17:32:41 -0400

    initial commit

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -18,16 +18,20 @@ class Navigation extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="my_cars">My Cars</Nav.Link>
-            <Nav.Link href="loanCalculator">Loan Calculator</Nav.Link>
+            <Nav.Link href="/#my_cars">My Cars</Nav.Link>
+            <Nav.Link href="/#loanCalculator">Loan Calculator</Nav.Link>
             <NavDropdown title="Resume" id="basic-nav-dropdown">
-              <NavDropdown.Item href="experience">Experience</NavDropdown.Item>
-              <NavDropdown.Item href="projects">Projects</NavDropdown.Item>
-              <NavDropdown.Item href="full_resume">
+              <NavDropdown.Item href="/#experience">
+                Experience
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/#projects">Projects</NavDropdown.Item>
+              <NavDropdown.Item href="/#full_resume">
                 Full resume
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="contact_me">Contact me</NavDropdown.Item>
+              <NavDropdown.Item href="/#contact_me">
+                Contact me
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
